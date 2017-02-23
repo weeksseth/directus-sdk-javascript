79c1bb43bcde8eca7ce9656e55d09f21c2225b72
Rijk van Zanten <rijkvanzanten@me.com>
2017-02-23 21:23:10 +0100

    :construction: start work on getTable()

---
+++ src/local-methods/tables.js
@@ -0,0 +1,62 @@
+module.exports = {
+  /**
+   * Create a new table
+   * @param {String} tableName
+   * @return {Promise} resolves something
+   */
+  createTable(tableName) {
+    return new Promise((reject, resolve) => {
+      // Create new table
+      this.knex.schema.createTable(tableName, table => {
+        table.increments();
+        table.integer('active');
+      })
+        .then(table => resolve(table))
+        .catch(err => reject(err));
+
+      // Add default privilege to directus_privileges
+      this.knex('directus_privileges')
+        .insert({
+          table_name: tableName,
+          allow_view: 2,
+          allow_add: 1,
+          allow_edit: 2,
+          allow_delete: 2,
+          allow_alter: 1,
+          group_id: 1,
+          nav_listed: 1,
+          status_id: null
+        });
+    });
+  },
+
+  /**
+   * Get all table names
+   * @param {Object} params options
+   */
+  getTables(params = {}) {
+    return new Promise((reject, resolve) => {
+      /**
+       * Knex can't access information_schema since it's bound to the
+       *   directus table
+       */
+      this.knex('directus_privileges')
+        .select('table_name')
+        .then(res => res.map(row => row.table_name))
+        .then(res => {
+          return params.include_system ? res : res.filter(row => !row.startsWith('directus_'));
+        })
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });
+  },
+
+  /**
+   * Get table info
+   */
+  getTable(tableName, params = {}) {
+    return new Promise((reject, resolve) => {
+
+    });
+  }
+};
